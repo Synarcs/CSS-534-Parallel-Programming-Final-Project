361bb11582349182955d147e99a51b6cbc1d6860
Vedang Parasnis <vedangparasnis@Vedangs-MacBook-Air.local>
2023-11-26 20:14:53 -0800

    added global combine reducer

--- mapreduce/src/main/java/com/css534/parallel/FacilityCombinerReducer.java
+++ mapreduce/src/main/java/com/css534/parallel/FacilityCombinerReducer.java
@@ -2,13 +2,18 @@ package com.css534.parallel;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class FacilityCombinerReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+import static com.css534.parallel.DelimeterRegexConsts.FACILITY_TYPE;
+
+public class FacilityCombinerReducer extends MapReduceBase implements Reducer<IntWritable, GlobalSkylineObjects, Text, Text> {
     private JobConf conf;
     private Log log = LogFactory.getLog(FacilityCombinerReducer.class);
 
@@ -18,40 +23,40 @@ public class FacilityCombinerReducer extends MapReduceBase implements Reducer<Te
         this.conf = job;
     }
 
+
     @Override
-    public void reduce(Text facilityName, Iterator<Text> iterator, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+    public void reduce(IntWritable facilityNameColumn, Iterator<GlobalSkylineObjects> iterator, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
 
-        Set<Vector2f> uniqueLocalSkylineObjects = new LinkedHashSet<>();
-        StringBuilder ans = new StringBuilder();
-        // // O(nlogn)
-        while (iterator.hasNext()){
-            String skylineObjectPayload = iterator.next().toString();
-            skylineObjectPayload = skylineObjectPayload.trim().replace("[", "").replace("]", "").trim();
-            double[] objectPayload = Arrays.stream(skylineObjectPayload.split(","))
-                    .mapToDouble(Double::parseDouble)
-                    .toArray();
+         int colNumber = facilityNameColumn.get();
+         Set<Double> favourable = new HashSet<>();
+         Set<Double> unfavourable = new HashSet<>();
 
-            for (int i = 0; i < objectPayload.length; i+=2 ){
-                uniqueLocalSkylineObjects.add(
-                        new Vector2f(
-                                objectPayload[i], objectPayload[i+1]
-                        )
-                );
-            }
-        }
+         // later implement hadoop batch Processing
+        Pattern facilityTypeRegex = Pattern.compile(FACILITY_TYPE);
+
+         while (iterator.hasNext()) {
+             String facilityType = iterator.next().getFacilityType();
+             double xRowProjection = iterator.next().getxProjections();
+             Matcher facilityTypeMatch = facilityTypeRegex.matcher(facilityType);
+             if (facilityTypeMatch.find()) {
+                 unfavourable.add(xRowProjection);
+             } else {
+                 favourable.add(xRowProjection);
+             }
+         }
+
+         favourable.removeAll(unfavourable);
 
         StringBuilder builder = new StringBuilder();
 
-        uniqueLocalSkylineObjects.stream().forEach((value) -> {
-            builder.append(value.getXx() + "," + value.getYy());
+        favourable.stream().forEach((value) -> {
+            builder.append(favourable + "," + colNumber);
             builder.append(",");
         });
 
-        String data = builder.substring(0, builder.length() - 1);
-
         outputCollector.collect(
-                new Text(facilityName),
-                new Text(data.toString())
+                new Text(String.valueOf(colNumber)),
+                new Text(builder.toString())
         );
     }
 }
